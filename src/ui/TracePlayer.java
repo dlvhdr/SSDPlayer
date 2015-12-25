be9aa94030a40a541318d2d64e441b012cb0f70b
Dolev Hadar <dolevc2@gmail.com>
2015-12-25 15:17:15 +0200

    Added reading of breakpoints.xml file and initializing trace player with it
    Signed-off-by: Dolev Hadar <dolevc2@gmail.com>

--- src/ui/TracePlayer.java
+++ src/ui/TracePlayer.java
@@ -104,7 +104,7 @@ public class TracePlayer extends JPanel {
 
 	private Device<?,?,?,?> currentDevice;
 	
-	private List<IBreakpoint> breakpoints = new ArrayList<IBreakpoint>();
+	private List<IBreakpoint> breakpoints;
 	private ManageBreakpointsDialog breakpointsDialog;
 	
     public TracePlayer(VisualConfig visualConfig, TwoObjectsCallback<Device<?, ?, ?, ?>, Iterable<StatisticsGetter>> resetDevice, OneObjectCallback<Device<?,?,?,?>> updateDevice) {
@@ -139,6 +139,11 @@ public class TracePlayer extends JPanel {
 //			stopLabel.setText("0");
 //			setProgressBarFrame(0);
 		}
+	}
+	
+	public void setInitialBreakpoints(List<IBreakpoint> initialBreakpoints) {
+		breakpoints = new ArrayList<IBreakpoint>();
+		breakpoints.addAll(initialBreakpoints);
 	}
 	
 	private void initManagerSelection() {
